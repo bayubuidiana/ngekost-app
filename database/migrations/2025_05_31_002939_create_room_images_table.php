240a41038836b51a4801bc44f9ae56cc293eb18d
bayubudiana <bayubudiana93@gmail.com>
2025-06-02 20:26:05 +0800

    data base fix

--- database/migrations/2025_05_31_002939_create_room_images_table.php
+++ database/migrations/2025_05_31_002939_create_room_images_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('room_images', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('room_id')->constrained();
+            $table->foreignId('room_id')->constrained()->cascadeOnDalate();
             $table->string('image');
             $table->timestamps();
         });
--- database/migrations/2025_05_31_002955_create_bonuses_table.php
+++ database/migrations/2025_05_31_002955_create_bonuses_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('bonuses', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('boarding_house_id')->constrained();
+            $table->foreignId('boarding_house_id')->constrained()->cascadeOnDalate();
             $table->string('image');
             $table->string('name');
             $table->string('description');
